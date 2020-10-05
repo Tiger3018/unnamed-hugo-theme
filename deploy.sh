6e6e1d182b92fd124149408b11f18e4819696dec
Tiger3018 <tiger3018of02@gmail.com>
2020-10-06 00:38:39 +0800

    test Travis CI

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env sh
+
+sitefile="exampleSite"
+themename="plain"
+
+mkdir -p workplace/themes/$themename
+cp -R -f $sitefile/* workplace
+for file in `ls`; do
+  if [ $file == $sitefile ] || [ $file == workplace ]; then
+    continue;
+  fi;
+  cp -R -f $file workplace/themes/$themename
+done;
+
+cd workplace
+hugo --debug -t $themename
